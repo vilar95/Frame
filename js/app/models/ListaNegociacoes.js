048aaf99d592d02a37f11637747ba7a25b77ace6
EDUARDO VILAR <97699470+vilar95@users.noreply.github.com>
2022-07-14 16:09:33 -0300

    Add files via upload

---
+++ js/app/models/ListaNegociacoes.js
@@ -0,0 +1,14 @@
+class ListaNegociacoes{
+    constructor() {
+        this._negocicacoes = [];
+    }
+
+    adiciona(negociacao) {
+        this._negocicacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+        
+        return [].concat(this._negocicacoes);
+    }
+}
